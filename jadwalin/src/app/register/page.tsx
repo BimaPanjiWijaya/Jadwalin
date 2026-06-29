2f892ee451df2201f088c29ebacdda410b54a415
unknown <bimapanjiwijaya@gmail.com>
2026-06-29 16:42:06 +0700

    done setup struktur register

--- jadwalin/src/app/register/page.tsx
+++ jadwalin/src/app/register/page.tsx
@@ -3,4 +3,34 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-export default function RegisterPage() {}
+export default function RegisterPage() {
+  const router = useRouter();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: "CUSTOMER",
+  });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+
+    const res = await fetch("api/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form),
+    });
+    const data = await res.json();
+
+    if (!res.ok) {
+      setError(data.error);
+      setLoading(false);
+      return;
+    }
+    router.push("/");
+  }
+}
