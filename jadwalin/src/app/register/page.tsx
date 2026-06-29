383c80128aa5d6464cde910d548081b6be68e13a
unknown <bimapanjiwijaya@gmail.com>
2026-06-29 17:20:06 +0700

    done: register page

--- jadwalin/src/app/register/page.tsx
+++ jadwalin/src/app/register/page.tsx
@@ -9,7 +9,7 @@ export default function RegisterPage() {
     name: "",
     email: "",
     password: "",
-    role: "CUSTOMER",
+    role: "",
   });
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
@@ -33,5 +33,66 @@ export default function RegisterPage() {
     }
     router.push("/");
   }
-  return <div>HELOO BOSS</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 w-full max-w-sm p-6 border rounded-lg"
+      >
+        <h1 className="text-2xl font-bold text-center">Register</h1>
+
+        {error && <p className="text-red-500 text-sm">{error}</p>}
+
+        <input
+          type="text"
+          placeholder="Nama Lengkap"
+          value={form.name}
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+          className="border rounded px-3 py-2"
+        />
+
+        <input
+          type="text"
+          placeholder="Email"
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          className="border rounded px-3 py-2"
+        />
+
+        <input
+          type="text"
+          placeholder="Password"
+          value={form.password}
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+          className="border rounded px-3 py-2"
+        />
+
+        <select
+          value={form.role}
+          onChange={(e) => setForm({ ...form, role: e.target.value })}
+          className="border rounded px-3 py-2"
+        >
+          <option value="" disabled={true}>
+            Pilih Role
+          </option>
+          <option value="CUSTOMER"> Customer</option>
+          <option value="BUSINESS_OWNER">Business Owner</option>
+        </select>
+
+        <button
+          type="submit"
+          disabled={loading}
+          className="bg-blue-600 text-white rounded py-2 disabled:opacity-50"
+        >
+          {loading ? "Memperoses..." : "Daftar"}
+        </button>
+        <p className="text-sm text-center">
+          Sudah punya akun?{" "}
+          <a href="/login" className="text-blue-600 underline">
+            Login
+          </a>
+        </p>
+      </form>
+    </div>
+  );
 }
