044c18c964c7a5578f7a8ce5c54d7d7c66827cab
unknown <bimapanjiwijaya@gmail.com>
2026-07-02 15:09:56 +0700

    setup loadiing page

--- jadwalin/src/app/login/page.tsx
+++ jadwalin/src/app/login/page.tsx
@@ -1,6 +1,29 @@
-// import { useState } from "react";
-// import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Loginpage() {
-  return <div>LOGIN PAGE</div>;
+  const router = useRouter();
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+
+    const res = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form),
+    });
+    const data = await res.json();
+
+    if (!res.ok) {
+      setError(data.error);
+      setLoading(false);
+      return;
+    }
+    router.push("/");
+  }
 }
