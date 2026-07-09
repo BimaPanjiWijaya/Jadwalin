a41ee4b7562cc8b6580a7a8192203fd6b7e412f1
unknown <bimapanjiwijaya@gmail.com>
2026-07-09 23:29:19 +0700

    refactor: remove unused font imports and simplify RootLayout component

--- jadwalin/src/app/layout.tsx
+++ jadwalin/src/app/layout.tsx
@@ -3,3 +3,29 @@ import { Geist } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+
+const geist = Geist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jadwalin - Booking & Scheduling",
+  description: "Platform booking layanan terpercaya",
+};
+
+export default function Rootlayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="id">
+      <body
+        className={`${geist.className}
+      bg-gray-50 min-h-screen flex flex-col`}
+      >
+        <Navbar />
+        <div className="flex-1">{children}</div>
+        <Footer />
+      </body>
+    </html>
+  );
+}
